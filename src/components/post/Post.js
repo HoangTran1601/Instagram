ea0425d3619d6587463c706f63042ba5ebfc956f
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-01-25 17:43:00 +0700

    Layout form Post Component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -11,16 +11,38 @@ import PostInfo from './PostInfo'
 export default class App extends Component<{}> {
   render() {
     return (
-      <View >
-        <PostHeader />
-        <PostImage />
-        <PostActivity />
-        <PostInfo />
+      <View style={styles.container}>
+        <View style={[styles.postHeader, styles.postItemLayout]}>
+          <PostHeader />
+        </View>
+
+        <View style={[styles.postImage]}>
+          <PostImage />
+        </View>
+
+        <View style={[styles.postActivity, styles.postItemLayout]}>
+          <PostActivity />
+        </View>
+
+        <View style={[styles.postInfo, styles.postItemLayout]}>
+          <PostInfo />
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginBottom: 15
+  },
+  postItemLayout: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginBottom: 15
+  },
+  postImage: {
+    marginBottom: 15
+  }
 });
 
