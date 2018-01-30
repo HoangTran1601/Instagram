fb0083ad9b64da13b8ababf91836a597f58e1dfe
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-01-30 17:33:22 +0700

    Revert "Commit"
    This reverts commit f7b81ae65025e161132b71a507a67d2836e4aa98.

--- App.js
+++ App.js
@@ -11,19 +11,12 @@ import {
 import Home from './src/screens/Home'
 import Signup from './src/screens/Signup'
 import Login from './src/screens/Login'
-<<<<<<< HEAD
 import CommentList from './src/components/comment/CommentList'
 import UserHeader from './src/components/user/UserHeader'
 export default class App extends Component<{}> {
   render() {
     return (
       <UserHeader />
-=======
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <Login />
->>>>>>> parent of 1822680... Done layout CommentList Component and test it in App.js
     );
   }
 }
