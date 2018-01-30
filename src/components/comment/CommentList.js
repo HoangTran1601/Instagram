11bed7638d8c5626345c2c9b3c9b78b19db2ce3f
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-01-30 11:17:47 +0700

    Setup Comment and CommentList Component

---
+++ src/components/comment/CommentList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback
+} from 'react-native';
+import Swiper from 'react-native-swiper'
+export default class App extends Component<{}> {
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+});
