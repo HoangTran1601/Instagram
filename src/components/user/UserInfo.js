24208d5fb9ee40ee40598baed3b9eb43259ecab1
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-02-05 20:29:02 +0700

    Init UserInfo Component

---
+++ src/components/user/UserInfo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
+
