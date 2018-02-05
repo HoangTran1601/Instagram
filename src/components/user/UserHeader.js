07bc8ed00a57328b87b86da52c43b282f6ec2d9d
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-02-05 20:59:56 +0700

    Modified size of avatar

--- src/components/user/UserHeader.js
+++ src/components/user/UserHeader.js
@@ -52,19 +52,17 @@ const styles = StyleSheet.create({
     marginRight: 20
   },
   image: {
-    width: 80, 
-    height: 80,
-    borderRadius: 40
+    width: 90, 
+    height: 90,
+    borderRadius: 45
   },
   generalInfo: {
     flex: 2
   },
   generalInfoContent: {
-    // backgroundColor: 'red',
     flexDirection: 'column'    
   },
   amount: {
-    // backgroundColor: 'blue',
     justifyContent: 'space-between',
     flexDirection: 'row',
   },
