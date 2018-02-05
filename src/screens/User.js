dfd3419a0c1304d07a4223fa6cafde1d3b6a7110
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-02-05 23:29:16 +0700

    Adding FlatList in User Screen

--- src/screens/User.js
+++ src/screens/User.js
@@ -6,53 +6,109 @@ import {
   Image,
   TouchableWithoutFeedback,
   TextInput,
-  ScrollView
+  ScrollView,
+  FlatList
 } from 'react-native';
 import UserHeader from '../components/user/UserHeader'
 import UserGallery from '../components/user/UserGallery'
 import UserInfo from '../components/user/UserInfo'
 import TabNavigator from 'react-native-tab-navigator'
 import PostList from '../components/post/PostList'
+import UserImage from '../components/user/UserImage'
+
 export default class App extends Component<{}> {
   constructor (props) {
     super(props)
     this.state = {
+      data: [
+        {key: 'Devin'},
+        {key: 'Jackson'},
+        {key: 'James'},
+        {key: 'Devin1'},
+        {key: 'Jackson1'},
+        {key: 'James1'},
+        {key: 'Devin2'},
+        {key: 'Jackson2'},
+        {key: 'James2'},
+        {key: 'Devin3'},
+        {key: 'Jackson3'},
+        {key: 'James3'},
+        {key: 'Devin3'},
+        {key: 'Jackson4'},
+        {key: 'James4'},
+      ],
+      isLoading: false,
+      isFooterLoading: false,
       selectedTab: 'home'
     }
   }
-  render() {
+  scroll () {
+    this.setState({isLoading: true})
+    setTimeout(() => {
+      this.setState({isLoading: false})
+    }, 4000)
+  }
+
+  end () {
+    // alert('ngan')
+    this.setState({isFooterLoading: true})
+    setTimeout(() => {
+      this.setState({isFooterLoading: false})
+    }, 4000)
+  }
+  renderHeader = () => {
+    // if (!this.state.isFooterLoading) return null;
+
     return (
-      <View style={styles.container}>
+      <View>
         <View style={styles.userHeader}>
           <UserHeader />
         </View>
+
         <View style={styles.userInfo}>
           <UserInfo />
         </View>
         <View style={styles.tabBar}>
-        <TabNavigator tabBarStyle={styles.tabBarStyle}>
-          <TabNavigator.Item
-            selected={this.state.selectedTab === 'home'}
-            renderIcon={() => <Image style={styles.image} source={require('../assets/img/image_icon.png')} />}
-            renderSelectedIcon={() => <Image style={styles.image} source={require('../assets/img/image_icon_fill.png')} />}
-            // badgeText="1"
-            onPress={() => this.setState({ selectedTab: 'home' })}>
-            {<UserGallery />}
-          </TabNavigator.Item>
-          
-          <TabNavigator.Item
-            selected={this.state.selectedTab === 'add'}
-            renderIcon={() => <Image style={styles.image} source={require('../assets/img/post_icon.png')} />}
-            renderSelectedIcon={() => <Image style={styles.image} source={require('../assets/img/post_icon_fill.png')} />}
-            // renderBadge={() => <CustomBadgeView />}
-            onPress={() => this.setState({ selectedTab: 'add' })}>
-            {<PostList />}
-          </TabNavigator.Item>
-        </TabNavigator>
+          <View style={styles.tabItem}>
+            <TouchableWithoutFeedback>
+              <Image
+                style={styles.icon}
+                source={require('../assets/img/image_icon.png')}
+              />
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={styles.tabItem}>
+            <TouchableWithoutFeedback>
+              <Image
+                style={styles.icon}
+                source={require('../assets/img/post_icon.png')}
+              />
+            </TouchableWithoutFeedback>
+          </View>
         </View>
       </View>
     );
+  };
+  render() {
+    const {isLoading} = this.state
+    return (
+      <View style={styles.container}>
+        <FlatList 
+          numColumns={3}
+          onRefresh={this.scroll.bind(this)}
+          refreshing={isLoading}
+          onEndReachedThreshold={1}
+          onEndReached={this.end.bind(this)}
+          data={this.state.data}
+          keyExtractor={item => item.key}
+          ListHeaderComponent={this.renderHeader}
+          renderItem={({item}) => <UserImage />}
+        />
+      </View>
+    );
   }
+
+ 
 }
 
 const styles = StyleSheet.create({
@@ -68,19 +124,21 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10
   },
-  image: {
+  icon: {
     width: 25,
     height: 25
   },
-  tabBarStyle: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'white'
-  },
   tabBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginTop: 25,
+    borderColor: 'rgb(240,240,240)',
+    borderTopWidth: 1
+  },
+  tabItem: {
     flex: 1,
-    marginTop: 25
+    alignItems: 'center'
   }
 });
