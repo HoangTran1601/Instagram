120ced9febd0420a2980fa15546324b19626aebe
Tran Thai Minh Hoang <minhhoang160196@gmail.com>
2018-02-05 21:00:46 +0700

    UserInfo Component

--- src/components/user/UserInfo.js
+++ src/components/user/UserInfo.js
@@ -10,13 +10,34 @@ export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        
+        <View>
+          <Text style={styles.username}>Ronal979</Text>
+        </View>
+        <View>
+          <Text style={styles.bio}>Something will happen</Text>
+        </View>
+        <View>
+          <Text style={styles.website}>www.never.com</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  username: {
+    fontWeight:'600',
+    fontSize: 15
+  },
+  bio: {
+    fontSize: 15,
+    lineHeight: 20
+  },
+  website: {
+    fontSize: 15,
+    color: 'rgb(0,0,238)',
+    textDecorationLine: 'underline',
+    textDecorationColor: 'rgb(0,0,238)'
+  }
 });
 
